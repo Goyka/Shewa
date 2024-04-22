eb0e0ca5c5dd5069556e025e11e7fc32d3b983ea
Goya <gim.goya@gmail.com>
2024-04-22 17:50:17 +0900

    add recent activity card, update useGraphConfigs.ts

---
+++ client/src/utils/useGraphConfigs.ts
@@ -0,0 +1,162 @@
+import { basic, theme } from "../design/colors/colorCodes";
+
+const vo2Data = [
+  { month: "Nov", value: 33 },
+  { month: "Dec", value: 35.6 },
+  { month: "Jan", value: 36.2 },
+  { month: "Feb", value: 37.2 },
+  { month: "Mar", value: 39.3 },
+  { month: "Apr", value: 41.9 },
+];
+
+const mileData = [
+  { month: "Nov", value: 62.3 },
+  { month: "Dec", value: 98.5 },
+  { month: "Jan", value: 63.3 },
+  { month: "Feb", value: 103.1 },
+  { month: "Mar", value: 90.0 },
+  { month: "Apr", value: 77.1 },
+];
+
+const pbData = [
+  { month: "Nov", value: 37, goal: 25 },
+  { month: "Dec", value: 38, goal: 25 },
+  { month: "Jan", value: 36, goal: 25 },
+  { month: "Feb", value: 31, goal: 25 },
+  { month: "Mar", value: 30, goal: 25 },
+  { month: "Apr", value: 26, goal: 25 },
+];
+
+const fatData = [
+  { month: "Nov", value: 20, goal: 16 },
+  { month: "Dec", value: 20, goal: 16 },
+  { month: "Jan", value: 19, goal: 16 },
+  { month: "Feb", value: 18, goal: 16 },
+  { month: "Mar", value: 17, goal: 16 },
+  { month: "Apr", value: 17, goal: 16 },
+];
+
+export const useGraphConfigs = () => {
+  const mileConfig = {
+    data: mileData,
+    width: 170,
+    height: 150,
+    marginTop: 20,
+    marginLeft: 6,
+    xField: "month",
+    yField: "value",
+    style: {
+      fill: ({ value }: any) => {
+        if (value < "99") {
+          return basic.lightgray;
+        }
+        return theme.blue;
+      },
+    },
+    label: {
+      text: ({ originData }: any) => {
+        const val = parseFloat(originData.value);
+        if (val < 0.05) {
+          return (val * 100).toFixed(1) + "%";
+        }
+        return "";
+      },
+      offset: 10,
+    },
+    legend: false,
+  };
+
+  const vo2Config = {
+    data: vo2Data,
+    width: 170,
+    height: 150,
+    marginTop: 20,
+    marginLeft: 6,
+    xField: "month",
+    yField: "value",
+    point: {
+      shapeField: "circle",
+      sizeField: 3,
+    },
+    interaction: {
+      tooltip: {
+        marker: false,
+      },
+    },
+    style: {
+      lineWidth: 2,
+    },
+  };
+
+  const pbConfig = {
+    data: pbData,
+    width: 170,
+    height: 150,
+    marginTop: 20,
+    marginLeft: 6,
+    xField: "month",
+    yField: "value",
+    annotations: [
+      {
+        type: "lineY",
+        data: [30],
+        style: { arrow: true, stroke: theme.red, lineDash: [2, 1] },
+        label: {
+          text: "goal",
+          position: "bottom",
+          style: { textBaseline: "bottom" },
+        },
+        tooltip: false,
+      },
+    ],
+    point: {
+      shapeField: "circle",
+      sizeField: 3,
+    },
+    interaction: {
+      tooltip: {
+        marker: false,
+      },
+    },
+    style: {
+      lineWidth: 2,
+    },
+  };
+
+  const fatConfig = {
+    data: fatData,
+    width: 170,
+    height: 150,
+    marginTop: 20,
+    marginLeft: 6,
+    xField: "month",
+    yField: "value",
+    annotations: [
+      {
+        type: "lineY",
+        data: [15],
+        style: { arrow: true, stroke: theme.red, lineDash: [2, 1] },
+        label: {
+          text: "goal",
+          position: "bottom",
+          style: { textBaseline: "bottom" },
+        },
+        tooltip: false,
+      },
+    ],
+    point: {
+      shapeField: "circle",
+      sizeField: 3,
+    },
+    interaction: {
+      tooltip: {
+        marker: false,
+      },
+    },
+    style: {
+      lineWidth: 2,
+    },
+  };
+
+  return { mileConfig, vo2Config, pbConfig, fatConfig };
+};
--- client/src/views/Dashboard.tsx
+++ client/src/views/Dashboard.tsx
@@ -1,184 +1,98 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Avatar, Card, Typography } from "antd";
-import { basic, theme } from "../design/colors/colorCodes";
+import { Avatar, Card, Typography, Image } from "antd";
 import { Line, Column } from "@ant-design/charts";
+import { theme } from "../design/colors/colorCodes";
+import { useGraphConfigs } from "../utils/useGraphConfigs";
 
 export default function Dashboard() {
-  const [activitiesData, setActivitiesData] = useState<any>();
+  // const [activitiesData, setActivitiesData] = useState<any>();
 
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
   const { Title, Text } = Typography;
+  const { mileConfig, vo2Config, pbConfig, fatConfig } = useGraphConfigs();
 
   useEffect(() => {
     // !token ? navigate("/landing") :
   }, []);
 
-  const mileConfig = {
-    data: mileData,
-    width: 170,
-    height: 150,
-    marginTop: 20,
-    marginLeft: 6,
-    xField: "month",
-    yField: "value",
-    style: {
-      fill: ({ value }: any) => {
-        if (value < "90") {
-          return theme.green;
-        }
-        return theme.blue;
-      },
-    },
-    label: {
-      text: ({ originData }: any) => {
-        const val = parseFloat(originData.value);
-        if (val < 0.05) {
-          return (val * 100).toFixed(1) + "%";
-        }
-        return "";
-      },
-      offset: 10,
-    },
-    legend: false,
-  };
-  const vo2Config = {
-    data: vo2Data,
-    width: 170,
-    height: 150,
-    marginTop: 20,
-    marginLeft: 6,
-    xField: "month",
-    yField: "value",
-    point: {
-      shapeField: "circle",
-      sizeField: 3,
-    },
-    interaction: {
-      tooltip: {
-        marker: false,
-      },
-    },
-    style: {
-      lineWidth: 2,
-    },
-  };
-  const pbConfig = {
-    data: pbData,
-    width: 170,
-    height: 150,
-    marginTop: 20,
-    marginLeft: 6,
-    xField: "month",
-    yField: "value",
-    annotations: [
-      {
-        type: "lineY",
-        data: [30],
-        style: { arrow: true, stroke: theme.red, lineDash: [2, 1] },
-        label: {
-          text: "goal",
-          position: "bottom",
-          style: { textBaseline: "bottom" },
-        },
-        tooltip: false,
-      },
-    ],
-    point: {
-      shapeField: "circle",
-      sizeField: 3,
-    },
-    interaction: {
-      tooltip: {
-        marker: false,
-      },
-    },
-    style: {
-      lineWidth: 2,
-    },
-  };
-  const fatConfig = {
-    data: fatData,
-    width: 170,
-    height: 150,
-    marginTop: 20,
-    marginLeft: 6,
-    xField: "month",
-    yField: "value",
-    annotations: [
-      {
-        type: "lineY",
-        data: [15],
-        style: { arrow: true, stroke: theme.red, lineDash: [2, 1] },
-        label: {
-          text: "goal",
-          position: "bottom",
-          style: { textBaseline: "bottom" },
-        },
-        tooltip: false,
-      },
-    ],
-    point: {
-      shapeField: "circle",
-      sizeField: 3,
-    },
-    interaction: {
-      tooltip: {
-        marker: false,
-      },
-    },
-    style: {
-      lineWidth: 2,
-    },
-  };
   return (
     <React.Fragment>
-      <header className="py-4 flex justify-center h-[26%]">
+      <header className="flex flex-col justify-center items-center h-[48%]">
+        <div className="flex justify-around items-center w-full">
+          <div className="flex flex-col justify-center items-center">
+            <Avatar size={70} style={{ marginBottom: "4px" }}></Avatar>
+            <Title level={5} style={{ marginBottom: "-2px" }}>
+              nickname
+            </Title>
+            <Text>Road</Text>
+          </div>
+          <div className="flex flex-col justify-start items-start">
+            <Text>Mileage /month</Text>
+            <Text>Vo2Max</Text>
+            <Text>Personal Best (5k)</Text>
+            <Text>Body Fat %</Text>
+          </div>
+        </div>
         <Card
           style={{
             width: "95%",
-            padding: "16px",
-            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
+            height: "210px",
+            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+            marginTop: "20px",
           }}
         >
-          <div className="flex w-full gap-6">
-            <div className="w-[32%] flex flex-col justify-center items-center">
-              <Avatar size={70}></Avatar>
-              <Title level={5} style={{ marginBottom: "-2px" }}>
-                nickname
-              </Title>
-              <Text>Road</Text>
+          <div style={{ width: "100%", paddingLeft: "8px" }}>
+            <Title level={5}>Recent Activity</Title>
+            <Text style={{ color: theme.blue, fontWeight: 700 }}>
+              "acitivity name"
+            </Text>
+          </div>
+          <div className="flex justify-start items-center px-2 gap-10 w-full">
+            <div className="flex flex-col justify-center items-center">
+              <Image
+                width={110}
+                style={{
+                  borderRadius: "8px",
+                  overflow: "hidden",
+                  width: "110px",
+                  height: "110px",
+                }}
+                src="https://www.littlethings.info/wp-content/uploads/2014/04/dummy-image-green-e1398449160839.jpg"
+              />
             </div>
-            <div className="w-[68%] flex flex-col justify-start items-start">
-              <Text>Mileage /month</Text>
-              <Text>Vo2Max</Text>
-              <Text>Personal Best (5k)</Text>
-              <Text>Body Fat %</Text>
+            <div className="flex flex-col justify-start items-start">
+              <Text>Workout Time</Text>
+              <Text>Distance</Text>
+              <Text>Elevation Gain</Text>
+              <Text>Avg. Pace</Text>
+              <Text>Avg. Heart Rate</Text>
+              <Text>Avg. Power</Text>
             </div>
           </div>
         </Card>
       </header>
-      <main className="flex flex-col gap-4 justify-start items-center h-[74%]">
+      <main className="flex flex-col gap-4 justify-start items-center h-[54%]">
         <div className="flex gap-4 justify-center items-center w-full">
           <Card
             style={{
               width: "45%",
               height: "170px",
-              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
+              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
             }}
           >
-            <Text>Mileage /month</Text>
+            <Text>Mileage (km/month)</Text>
             <Column {...mileConfig} />
           </Card>
           <Card
             style={{
               width: "45%",
               height: "170px",
-              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
+              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
             }}
           >
-            <Text>Vo2Max</Text>
+            <Text>Vo2Max (ml/kg/min)</Text>
             <Line colorField={theme.red} {...vo2Config} />
           </Card>
         </div>
@@ -187,60 +101,24 @@ export default function Dashboard() {
             style={{
               width: "45%",
               height: "170px",
-              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
+              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
             }}
           >
-            <Text>Personal Best (5k)</Text>
+            <Text>Personal Best (min/5k)</Text>
             <Line colorField={theme.yellow} {...pbConfig} />
           </Card>
           <Card
             style={{
               width: "45%",
               height: "170px",
-              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.2)",
+              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
             }}
           >
-            <Text>Body Fat %</Text>
-            <Line colorField={basic.lightgray} {...fatConfig} />
+            <Text>Body Fat (%)</Text>
+            <Line colorField={theme.green} {...fatConfig} />
           </Card>
         </div>
       </main>
     </React.Fragment>
   );
 }
-
-const vo2Data = [
-  { month: "Nov", value: 33 },
-  { month: "Dec", value: 35.6 },
-  { month: "Jan", value: 36.2 },
-  { month: "Feb", value: 37.2 },
-  { month: "Mar", value: 39.3 },
-  { month: "Apr", value: 41.9 },
-];
-
-const mileData = [
-  { month: "Nov", value: 62.3 },
-  { month: "Dec", value: 98.5 },
-  { month: "Jan", value: 63.3 },
-  { month: "Feb", value: 103.1 },
-  { month: "Mar", value: 90.0 },
-  { month: "Apr", value: 77.1 },
-];
-
-const pbData = [
-  { month: "Nov", value: 37, goal: 25 },
-  { month: "Dec", value: 38, goal: 25 },
-  { month: "Jan", value: 36, goal: 25 },
-  { month: "Feb", value: 31, goal: 25 },
-  { month: "Mar", value: 30, goal: 25 },
-  { month: "Apr", value: 26, goal: 25 },
-];
-
-const fatData = [
-  { month: "Nov", value: 20, goal: 16 },
-  { month: "Dec", value: 20, goal: 16 },
-  { month: "Jan", value: 19, goal: 16 },
-  { month: "Feb", value: 18, goal: 16 },
-  { month: "Mar", value: 17, goal: 16 },
-  { month: "Apr", value: 17, goal: 16 },
-];
